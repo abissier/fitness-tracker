81c11c3c75e0ed493346b90b1f23b8cede986065
abissier <71886314+abissier@users.noreply.github.com>
2021-03-01 10:27:59 -0800

    corrected model rows

--- models/workout.js
+++ models/workout.js
@@ -1,18 +1,25 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-    day: { type: Date },
-    exercises: {
-        type: { type: String },
-        name: { type: String },
-        duration: { type: Number },
-        weight: { type: Number },
-        rep: { type: Number },
-        set: { type: Number }
-    }
+	day: { type: Date, default: Date.now },
+	exercises: [
+		{
+			type: { type: String },
+			name: { type: String },
+			duration: { type: Number },
+			weight: { type: Number },
+			rep: { type: Number },
+			set: { type: Number },
+			distance: { type: Number }
+		}
+	],
+	totalDuration: {
+		type: Number,
+		default: 0
+	}
 });
 
-const Workout = mongoose.model("workout", ExerciseSchema);
+const Workout = mongoose.model('workout', ExerciseSchema);
 
 module.exports = Workout;
